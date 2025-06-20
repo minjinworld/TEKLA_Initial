7dc0b1bb59e183153755666d80b24e516e2cd19a
lee.minjin <lee_mimmi@naver.com>
2025-06-20 21:12:13 +0900

    Onboarding step scroll to top 적용

--- components/onboarding/StepNavigator.tsx
+++ components/onboarding/StepNavigator.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Step1 from "@/components/onboarding/Step1";
 import Step2 from "@/components/onboarding/Step2";
 import Step3 from "@/components/onboarding/Step3";
@@ -12,6 +12,11 @@ export default function StepsNavigator({ onComplete }: Props) {
 
   const goToNext = () => setStep((prev) => prev + 1);
   const goToPrev = () => setStep((prev) => prev - 1);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [step]);
+
   return (
     <>
       {step === 1 && <Step1 onNext={goToNext} />}
